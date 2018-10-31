2f545a19e24fd1e57d8da1e33ab7585c418658d9
Maxwell <maxperryg@gmail.com>
2018-10-31 19:43:53 -0400

    Switched from polynomial class to polynomial function

--- Polynomial/main.cpp
+++ Polynomial/main.cpp
@@ -12,40 +12,58 @@
 using namespace std;
 
 
-class Polynomial{
-private:
-    //if coefficient[3] is 5, that term willbe 5X^3
-    int coefficients[100];
-public:
-    Polynomial(){
-        for(int i=0;i<100; i++){
-            coefficients[i]=0;
-        }
+//class Polynomial{
+//private:
+//    //if coefficient[3] is 5, that term willbe 5X^3
+//    int coefficients[100];
+//public:
+//    Polynomial(){
+//        for(int i=0;i<100; i++){
+//            coefficients[i]=0;
+//        }
+//    }
+//    Polynomial(string p){
+//        for (int i=1; i<p.length(); i++) {
+//            int degree;
+//            stringstream(p.substr(i,1))>>degree;
+//            int coef;
+//            stringstream(p.substr(i-1,1))>>coef;
+//            coefficients[degree]=coef;
+//        }
+//        for(int i=0; i<10; i++){
+//            cout<<coefficients[i]<<"X"<<i<<endl;
+//        }
+//    }
+//};
+
+void constructPolynomial(int polynomial[], string line){
+    for (int i=1; i<line.length(); i++) {
+        int degree;
+        stringstream(line.substr(i,1))>>degree;
+        int coef;
+        stringstream(line.substr(i-1,1))>>coef;
+        polynomial[degree]=coef;
     }
-    Polynomial(string p){
-        
-        for (int i=1; i<p.length(); i++) {
-            int degree;
-            stringstream(p.substr(i,1))>>degree;
-            int coef;
-            stringstream(p.substr(i-1,1))>>coef;
-            coefficients[degree]=coef;
-        }
-        cout<<coefficients;
-    }
-};
+}
 
 int main() {
     string line;
-    ifstream myfile ("Numbers.txt");
-    if(myfile.is_open()){
-        while(getline(myfile,line)){
-            Polynomial left(line);
-            getline(myfile,line);
-            Polynomial right(line);
+    ifstream myfile ("/Users/MaxGrossman/Documents/C++WorkSpace/Polynomial/Polynomial/en.lproj/Numbers.txt");
+    while(getline(myfile,line)){
+        int left[100];
+        int right[100];
+        for(int i=0;i<100; i++){
+            left[i]=0;
+            right[i]=0;
         }
-        myfile.close();
+        constructPolynomial(left, line);
+        for(int i=0; i<10;i++){
+            cout<<left[i]<<"X"<<i<<endl;
+        }
+        getline(myfile,line);
+        constructPolynomial(right, line);
     }
+    myfile.close();
     
     return 0;
 }
